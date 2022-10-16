2eeb970e80e2568cd4fcb298f8344c4b547d7ae0
alan.xiao <alan.xiao@d1m.cn>
2022-10-16 10:53:45 +0800

    ✨新增(request):

--- webpack.config.js
+++ webpack.config.js
@@ -31,6 +31,41 @@ const config = {
   },
   module: {
     rules: [
+      {
+        test: /\.(gif|png|jpe?g|svg)$/i,
+        // type 属性适用于 Webpack5，旧版本可使用 file-loader
+        exclude: /node_modules/,
+        type: 'asset/resource',
+        use: [
+          {
+            loader: 'image-webpack-loader',
+            options: {
+              // 只在生产环境开启
+              disable: process.env.NODE_ENV === 'development',
+              // jpeg 压缩配置
+              mozjpeg: {
+                quality: 80
+              }
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(png|jpg)$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 1024
+            }
+          }
+        ]
+      },
+      {
+        test: /\.svg$/i,
+        use: ['raw-loader']
+      },
       { test: /\.pug$/, use: ['pug-plain-loader'] },
       { test: /\.vue$/, use: ['vue-loader'] },
       {
